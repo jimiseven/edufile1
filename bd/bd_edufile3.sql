4c47a00bc531fd4283c9d834e27c886a90064571
jimi tarks <tano777864@gmail.com>
2025-01-28 15:50:24 -0400

    last bd

--- bd/bd_edufile3.sql
+++ bd/bd_edufile3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-01-2025 a las 20:38:57
+-- Tiempo de generación: 28-01-2025 a las 20:49:56
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
